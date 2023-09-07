6b70001d628ece965e1bef9ebccd33a8c086ef6c
HarshitSaxena1 <harshitsaxena912003@gmail.com>
2023-09-07 11:29:04 +0530

    Initial commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navigation from "../components/navigation";
+import Hero from "../components/Hero";
+function App() {
+
+    return <div>
+    <Navigation />
+    <Hero />
+    </div>;
+}
+export default App;
